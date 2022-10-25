63d6dcd0e0564ddb67d4c0ca231f2bbbd09f2877
Patr-M <patryk.mantaj@infotech.edu.pl>
2022-10-25 20:41:57 +0200

    zad1-5

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,4 @@
-"""djangoProject URL Configuration
+"""projekcik URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from projekcik import views
 
 urlpatterns = [
+    path('',views.home_view),
     path('admin/', admin.site.urls),
+    path('polls/',include('polls.urls',namespace='polls'))
 ]
